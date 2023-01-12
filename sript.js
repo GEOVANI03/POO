b62663991d382c9e160a58f1abf8e7cb36535695
GEOVANI RODRIGUEZ JAIME <grj29644@gmail.com>
2023-01-12 02:29:24 -0600

    first commit

---
+++ sript.js
@@ -0,0 +1,7 @@
+function capturar(){
+    var name =document.ElementById("name").value;
+    var lastname = document.ElementById("lastname").value;
+    var years = document.ElementById("years").value;
+    alert(name + "  " + lastname + "  " +  years  );
+    
+}
